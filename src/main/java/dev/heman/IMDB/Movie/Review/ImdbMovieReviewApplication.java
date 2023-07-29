586a7de8f05ae6ab97eab9347f08b93a1b16e902
bhatiaheman <bhatiaheman8@gmail.com>
2023-07-29 14:10:35 +0530

    Spring boot + React

---
+++ src/main/java/dev/heman/IMDB/Movie/Review/ImdbMovieReviewApplication.java
@@ -0,0 +1,13 @@
+package dev.heman.IMDB.Movie.Review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbMovieReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbMovieReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/heman/IMDB/Movie/Review/ImdbMovieReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.heman.IMDB.Movie.Review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbMovieReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
